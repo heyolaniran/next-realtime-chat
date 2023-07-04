779f3d3ee6ce7e92fec275146f47bd0c5ce4b282
heyolaniran <olaniran.abd@gmail.com>
2023-07-04 13:25:38 +0100

    feat : adding loader animation

---
+++ client/src/Loader.js
@@ -0,0 +1,9 @@
+import load from './assets/img/message.gif' 
+export default function Loader() {
+  return (
+    <div className="mt-4 items-center ">
+       <img src={load} className="object-fit " width={400} height={400} />
+    </div>
+
+  )
+}
