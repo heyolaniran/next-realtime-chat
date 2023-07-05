5b3710da20090a4d7ffe53f46f36c922b281babb
heyolaniran <olaniran.abd@gmail.com>
2023-07-05 14:15:12 +0100

    feat: applying styles to the room messages

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -3,9 +3,8 @@ import { useState, useEffect } from "react"
 export default function Room({room , username , socket}) {
 
     const [currentMessage , setCurrentMessage] = useState("") ; 
-
+    const [messageList , setMessageList] = useState([]) ; 
     const sendMessage = async () =>{ 
-
         if(currentMessage !== "") { 
               const messageData = { 
                     room , 
@@ -15,16 +14,18 @@ export default function Room({room , username , socket}) {
                 }
 
                 await socket.emit("send_message" , messageData)
+                setMessageList((list) => [...list , messageData])
         }
       
     }
 
     useEffect(() => {
         socket.on("receive_message" , (data) => { 
-            console.log(data)
+           setMessageList((list) =>[...list , data])
         })
     }, [socket])
     
+   
 
 
   return (
@@ -34,18 +35,7 @@ export default function Room({room , username , socket}) {
             <div className="flex-1 bg-gray-100 rounded-xl shadow-lg w-full h-[60%] overflow-y-scroll">
                 <div className="main-body container m-auto w-11/12 h-full flex flex-col">
                     <div className="py-4 flex-2 flex flex-row">
-                        <div className="flex-1">
-                            <span className="xl:hidden inline-block text-gray-700 hover:text-gray-900 align-bottom">
-                                <span className="block h-6 w-6 p-1 rounded-full hover:bg-gray-400">
-                                    <svg className="w-4 h-4" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                                </span>
-                            </span>
-                            <span className="lg:hidden inline-block ml-8 text-gray-700 hover:text-gray-900 align-bottom">
-                                <span className="block h-6 w-6 p-1 rounded-full hover:bg-gray-400">
-                                    <svg className="h-4 w-4" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
-                                </span>
-                            </span>
-                        </div>
+                       
                         <div className="flex-1 text-right">
                             <span className="inline-block text-gray-700">
                                 Status: <span className="inline-block align-text-bottom w-4 h-4 bg-green-400 rounded-full border-2 border-white"></span> <b>Online</b>
@@ -69,49 +59,34 @@ export default function Room({room , username , socket}) {
                             <div className="chat-area flex-1 flex flex-col">
                                 <div className="messages flex-1 ">
                                
-                                    <div className="message mb-4 flex">
-                                        <div className="grid">
-                                        <p className="pl-4 font-normal text-gray-500 text-sm mb-2">@heyolaniran </p>
-                                        <div className="flex-1 px-2">
-                                            <div className="inline-block bg-gradient-to-r from-red-400 to-orange-600 rounded-full p-2 px-6 text-white w-full ">
-                                              <span>Hey there.  What about you?</span>
-                                            </div>
-                                            <div className="pl-4"><small className="text-gray-500">15 April </small></div>
-
-                                          
-                                        </div>
-                                        </div>
-                                       
-                                    </div>
+                                  
                                    
-                                    <div className="message me mb-4 flex text-right">
-                                        <div className="flex-1 px-2">
-                                            <div className="inline-block bg-blue-600 rounded-full p-2 px-6 text-white">
-                                                <span>It's like a dream come true</span>
-                                            </div>
-                                            <div className="pr-4"><small className="text-gray-500">15 April</small></div>
-                                        </div>
-                                    </div>
-                                    <div className="message me mb-4 flex text-right">
-                                        <div className="flex-1 px-2">
-                                            <div className="inline-block bg-blue-600 rounded-full p-2 px-6 text-white">
-                                                <span>I accept. Thank you very much.</span>
-                                            </div>
-                                            <div className="pr-4"><small className="text-gray-500">15 April</small></div>
-                                        </div>
-                                    </div>
-                                    <div className="message mb-4 flex">
+                                    {messageList.map((content) => { 
+                                      return content.author === username ? (
+
+                                       <div className="message me mb-4 flex text-right">
+                                          <div className="flex-1 px-2">
+                                              <div className="inline-block bg-blue-600 rounded-full p-2 px-6 text-white">
+                                                  <span>{content.message}</span>
+                                              </div>
+                                              <div className="pr-4"><small className="text-gray-500">{content.time}</small></div>
+                                          </div>
+                                      </div>
+                                       ) : (
+                                        <div className="message mb-4 flex">
                                         <div className="grid">
-                                        <p className="pl-4 font-normal text-gray-500 text-sm mb-2">@heyolaniran </p>
-                                        <div className="flex-1 px-2">
-                                            <div className="inline-block bg-gradient-to-r from-red-400 to-orange-600 rounded-full p-2 px-6 text-white w-full">
-                                                <span>You are welcome bruuuh. We will stay in touch.</span>
-                                            </div>
-                                            <div className="pl-4"><small className="text-gray-500">15 April</small></div>
+                                          <p className="pl-4 font-normal text-gray-500 text-sm mb-2">@{content.author}</p>
+                                          <div className="flex-1 px-2">
+                                              <div className="inline-block bg-gradient-to-r from-red-400 to-orange-600 rounded-full p-2 px-6 text-white w-full">
+                                                  <span>{content.message}</span>
+                                              </div>
+                                              <div className="pl-4"><small className="text-gray-500">{content.time}</small></div>
+                                          </div>
                                         </div>
                                         </div>
-                                      
-                                    </div>
+                                       )
+                                    })}
+                                    
                                 </div>
                             </div>
                         </div>
@@ -127,7 +102,9 @@ export default function Room({room , username , socket}) {
               placeholder="Write down your message ...  "
               value={currentMessage}
               onChange={(e) => setCurrentMessage(e.target.value)}
-              onMouseEnter={sendMessage}
+              onKeyDown={(e) => { 
+                e.key === "Enter" && sendMessage() 
+              }}
               className="w-full rounded-md border-gray-200 bg-white p-3 text-gray-700 shadow-sm transition focus:border-white focus:outline-none focus:ring focus:ring-blue-400"
             />
           </div>
@@ -143,6 +120,8 @@ export default function Room({room , username , socket}) {
           </button>
         </div>
         </div>
+
+     
        
       </>
   )
